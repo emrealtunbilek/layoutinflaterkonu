1452a22168dd0d3490c099bb6cd0bfe2e8a4d525
Emre Altunbilek <emrealtunbilek@gmail.com>
2017-12-25 12:44:41 +0300

    layout inflater 2. bölüm

--- app/src/main/java/com/emrealtunbilek/layoutinflationkonuanlatimi/MainActivity.kt
+++ app/src/main/java/com/emrealtunbilek/layoutinflationkonuanlatimi/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
         //dönüştürme işlemi yapılır ama bir root'a atanmaz, bunu kendimiz addview diyerek yapmamız gerekiyor.
-        var view=inflater.inflate(R.layout.layout_text_view, null)
+       // var view=inflater.inflate(R.layout.layout_text_view, null)
         //view nesnesinde textview var
         //root null
         //layoutparams null
@@ -31,6 +31,20 @@ class MainActivity : AppCompatActivity() {
 
         //Log.e("INFLATION", ""+view.parent) //rootlayout
         //Log.e("INFLATION", ""+view.layoutParams) //framelayoutun genişlik yükseklik değerlerine göre yerleştirilir
+
+        //var view=inflater.inflate(R.layout.layout_text_view, icerdekiLayout)
+        //var view=inflater.inflate(R.layout.layout_text_view, icerdekiLayout,true)
+
+
+        var view=inflater.inflate(R.layout.layout_text_view, icerdekiLayout,false )
+
+
+        Log.e("INFLATION", ""+view.parent) // null
+        Log.e("INFLATION", ""+view.layoutParams) // //linearlayout
+
+        icerdekiLayout.addView(view)
+
+
 
     }
 }
